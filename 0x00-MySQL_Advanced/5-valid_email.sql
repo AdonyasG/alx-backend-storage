145c3d21e5158902e666fe6cb87a77f1ed561b4a
AdonyasG <adonyas2000@gmail.com>
2023-04-06 18:42:35 -0400

    initial commit

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- create trigger before insert
+-- updaitng multiple tables using sql script
+CREATE TRiGGER reset_attribute
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF NEW.email <> OLD.email THEN
+    UPDATE users
+    SET valid_email = default_value;
+    END IF;
+END;
