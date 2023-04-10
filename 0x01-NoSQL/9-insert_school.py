e5cc1c887eb067a0077b9ce58d6eee893ccd5a8f
AdonyasG <adonyas2000@gmail.com>
2023-04-10 13:15:07 -0400

    initial commit

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module - insert_school
+"""
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts new document to a collection"""
+    collection = mongo_collection.insert_one(kwargs)
+    return collection.inserted_id
