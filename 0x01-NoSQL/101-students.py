3572468e161200b75df278e78e4e23af9ce24e88
AdonyasG <adonyas2000@gmail.com>
2023-04-11 07:26:22 -0400

    initial commit

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module - 101-students
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Use the aggregation framework to calculate
+    the average score for each student
+    """
+    pipeline = [
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ]
+    results = mongo_collection.aggregate(pipeline)
+    return results
