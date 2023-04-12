1e70b9d0c02dabd0ade9a21cfa50f8ba6c28bce8
AdonyasG <adonyas2000@gmail.com>
2023-04-12 16:46:06 -0400

    initial commit

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pyton3
+"""
+Module - web
+"""
+import requests
+import redis
+
+
+r = redis.Redis()
+
+
+def get_page(url: str) -> str:
+    """get page"""
+    count_key = f"count:{url}"
+    content_key = f"content:{url}"
+    content = r.get(content_key)
+
+    if content:
+        return content.decode('utf-8')
+    else:
+        response = requests.get(url)
+
+        content = response.content.decode('utf-8')
+        r.set(content_key, content, ex=10)
+
+        r.incr(count_key)
+
+        return content
