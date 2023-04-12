a694974d5a7f620587cb55467516d869f6fc8a74
AdonyasG <adonyas2000@gmail.com>
2023-04-12 07:07:16 -0400

    initial commit

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module - exercise
+"""
+import redis
+import uuid
+from typing import Any
+
+
+class Cache:
+    """cache in a redis storage"""
+    def __init__(self, host='localhost', port=6379) -> None:
+        """initialize"""
+        self._redis = redis.Redis(host=host, port=port)
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """store the data by setting key and data"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
